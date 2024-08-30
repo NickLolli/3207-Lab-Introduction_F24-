2865711d235e2dd6defb38e75818a70b951fc8b6
NickLolli <tup62287@temple.edu>
2024-08-30 01:03:45 -0400

    Update print_random.c

--- print_random.c
+++ print_random.c
@@ -1,7 +1,9 @@
-char* rand_string_alloc(size_t size) {
-    char *s = malloc(size + 1); 
-    if (s) {
-        rand_string(s, size); 
-    }
-    return s;
+//Given Project code
+char* rand_string_alloc(size_t size)
+{
+     char *s = malloc(size + 1);
+     if (s) {
+         rand_string(s, size);
+     }
+     return s;
 }
