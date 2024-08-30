b72638d5f4745fe65b2a0abd3b5d3ed1a80f8f84
NickLolli <tup62287@temple.edu>
2024-08-30 01:00:48 -0400

    Update print_random.c

--- print_random.c
+++ print_random.c
@@ -1,9 +1,24 @@
-//Given Project code
-char* rand_string_alloc(size_t size)
-{
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
+#include <stdlib.h>
+#include <time.h>
+
+// Random character from A to Z
+char randchar() {
+    srand(time(0));
+    return 'A' + (rand() % 26);
+}
+
+// Random string of size
+void rand_string(char *s, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        s[i] = randchar(); 
+    }
+    s[size] = '\0'; 
+}
+
+char* rand_string_alloc(size_t size) {
+    char *s = malloc(size + 1); 
+    if (s) {
+        rand_string(s, size); 
+    }
+    return s;
 }
